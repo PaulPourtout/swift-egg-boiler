71f83226a18f00f99c2345adcc4b3661161e2c8e
Paul <paul@kobusapp.com>
2020-09-20 19:37:17 +0200

    first commit

--- EggTimer/ViewController.swift
+++ EggTimer/ViewController.swift
@@ -9,7 +9,27 @@
 import UIKit
 
 class ViewController: UIViewController {
+    let cookTimes: [String: Int] = [ "Soft": 5, "Medium": 7, "Hard": 12 ];
+    var count: Int = 0
+    var currentTimer = Timer()
     
+    @IBOutlet weak var appLabel: UILabel!
+    @IBAction func hardnessSelected(_ sender: UIButton) {
+        let hardness = sender.currentTitle!
+        appLabel.text = "Your eggs are boiling..."
 
-
+        count = cookTimes[hardness]! //* 60
+        currentTimer = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(updateCounter), userInfo: nil, repeats: true)
+        
+    }
+    
+    @objc func updateCounter() {
+        if (count > 0) {
+            print(count)
+            count-=1
+        } else {
+            currentTimer.invalidate()
+            appLabel.text = "Done !"
+        }
+    }
 }
